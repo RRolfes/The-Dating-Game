ab0622bf6c40b0350c3fc7bdfeb3346fe0947fdd
ccomings <ccomings@gmail.com>
2017-08-13 12:24:13 -0700

    create frontend useractions, userupdatereducer

--- frontend/actions/index.js
+++ frontend/actions/index.js
@@ -1,3 +1,6 @@
+export * from './userActions';
+
+
 export const changeText = (text) => {
   return {
     type: 'CHANGE_TEXT',
---
+++ frontend/actions/userActions.js
@@ -0,0 +1,6 @@
+import { Actions } from 'react-native-router-flux';
+import {
+  USER_EDIT,
+  USER_FETCH_SUCESS,
+  USER_SAVE_SUCCESS
+} from './types';
