6171fccac082f32db084467e26c5c1ee04a6bde4
ccomings <ccomings@gmail.com>
2017-08-14 01:35:09 -0700

    switched to live environment, backend is now dead with this push

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,3 +1,3 @@
-const API_URL = 'mongodb://localhost/theDatingGame';
+const API_URL = 'https://damp-savannah-20880.herokuapp.com';
 
 export const USERS_URL = (user_id) => `${API_URL}/auth/users/${user_id}`;
